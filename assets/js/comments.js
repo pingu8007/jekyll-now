b5c127c5217615106780e493ab81661e9f900060
PinGu <pingu8007@gmail.com>
2020-07-09 11:15:21 +0800

    Update staticman configuration and layout

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,13 +1,18 @@
 function postCaptcha(token) {
   var el = document.getElementById("make_comment");
   if (el == null) return;
+  var hint = el.getElementsByClassName("hint")[0];
   var qs = new URLSearchParams();
   new FormData(el).forEach((v, k) => qs.append(k, v));
-  fetch(el.action, { body: qs, method: "POST", }).then(res => {
-    if (!res.ok) {
-      res.json().then(body => {
-        alert(body.message || body.errorCode || "Failed to post comments");
-      });
-    }
-  }).catch(err => console.log(err)).then(() => { grecaptcha.reset() })
+  fetch(el.action, { body: qs, method: "POST", }).then(res => res.json().then(body => {
+    if (!res.ok)
+      throw new Error(body.message || body.errorCode || "Failed to create comment");
+    hint.classList.remove("error");
+    hint.textContent = "Saved";
+    el.reset();
+  })).catch(e => {
+    console.error(e);
+    hint.classList.add("error");
+    hint.textContent = e.message;
+  }).then(() => grecaptcha.reset());
 }
