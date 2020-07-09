8a982d987572e23cd9858b3338d22596cd251375
PinGu <pingu8007@gmail.com>
2020-07-09 10:08:52 +0800

    Switch to reCaptcha invisible for better layout

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,15 +1,13 @@
-var commentForm = document.getElementById("make_comment");
-if (commentForm !== null) {
-  commentForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    var qs = new URLSearchParams();
-    new FormData(e.target).forEach((v, k) => qs.append(k, v));
-    fetch(e.target.action, { body: qs, method: "POST", }).then(res => {
-      if (!res.ok) {
-        res.json().then(body => {
-          alert(body.message || body.errorCode || "Failed to post comments");
-        });
-      }
-    }).catch(err => console.log(err)).then(() => { grecaptcha.reset() })
-  })
+function postCaptcha(token) {
+  var el = document.getElementById("make_comment");
+  if (el == null) return;
+  var qs = new URLSearchParams();
+  new FormData(el).forEach((v, k) => qs.append(k, v));
+  fetch(el.action, { body: qs, method: "POST", }).then(res => {
+    if (!res.ok) {
+      res.json().then(body => {
+        alert(body.message || body.errorCode || "Failed to post comments");
+      });
+    }
+  }).catch(err => console.log(err)).then(() => { grecaptcha.reset() })
 }
